0bfc4f4c9293149136774e283cba35b8b9dad5ce
okolie emmanuel <Leoemmanuel93@gmail.com>
2025-05-22 20:33:17 +0100

    search functionality improvement

--- countdown/countdown/settings.py
+++ countdown/countdown/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['candlesdown.onrender.com']
+ALLOWED_HOSTS = ['candlesdown.onrender.com','127.0.0.1']
 
 
 # Application definition
