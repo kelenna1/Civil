80d597ef635eb4b5d5ced0c3fcd4d5211776f2f9
okolie emmanuel <Leoemmanuel93@gmail.com>
2025-06-25 21:29:18 +0100

    custom error

--- countdown/birthday/views.py
+++ countdown/birthday/views.py
@@ -346,13 +346,13 @@ def unsubscribe(request, token):
 
 
 def custom_error_view(request, exception=None):
-    return render(request, '500.html', status=500)
+    return render(request, 'birthday/500.html', status=500)
 
 def custom_page_not_found_view(request, exception):
-    return render(request, '404.html', status=404)
+    return render(request, 'birthday/404.html', status=404)
 
 def custom_permission_denied_view(request, exception):
-    return render(request, '403.html', status=403)
+    return render(request, 'birthday/403.html', status=403)
 
 def custom_bad_request_view(request, exception):
-    return render(request, '400.html', status=400)
+    return render(request, 'birthday/400.html', status=400)
