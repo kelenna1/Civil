8dd6850c74ab1e920e4411fd3bfa8909ab118118
okolie emmanuel <Leoemmanuel93@gmail.com>
2025-06-26 03:22:11 +0100

    internal

--- countdown/birthday/views.py
+++ countdown/birthday/views.py
@@ -345,14 +345,14 @@ def unsubscribe(request, token):
     return render(request, 'birthday/dashboard.html')
 
 
-def custom_error_view(request, exception=None):
-    return render(request, 'birthday/500.html', status=500)
+# def custom_error_view(request, exception=None):
+#     return render(request, 'birthday/500.html', status=500)
 
-def custom_page_not_found_view(request, exception):
-    return render(request, 'birthday/404.html', status=404)
+# def custom_page_not_found_view(request, exception):
+#     return render(request, 'birthday/404.html', status=404)
 
-def custom_permission_denied_view(request, exception):
-    return render(request, 'birthday/403.html', status=403)
+# def custom_permission_denied_view(request, exception):
+#     return render(request, 'birthday/403.html', status=403)
 
-def custom_bad_request_view(request, exception):
-    return render(request, 'birthday/400.html', status=400)
+# def custom_bad_request_view(request, exception):
+#     return render(request, 'birthday/400.html', status=400)
